228ce2ec4ed9fb3b6791a5c2923d5e5e0d0fab64
Vitaliy Zhyrytskyy <vitaliy_zhyrytskyy@epam.com>
2022-08-01 14:48:19 +0300

    task2-comments

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -17,7 +17,7 @@ import { SharedModule } from '../shared/shared.module';
   ],
   exports: [
     CartListComponent,
-    CartItemComponent
+    // CartItemComponent
   ]
 })
 export class CartModule { }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -15,7 +15,7 @@ import { ProductComponent } from './components/product/product.component';
   ],
   exports: [
     ProductListComponent,
-    ProductComponent
+    // ProductComponent
   ]
 })
 export class ProductsModule { }
