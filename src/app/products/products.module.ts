bbb7d2c18d75b68d96bf0b6e3a09fc22a39444ef
raimovasad <81373435+asadbek2021@users.noreply.github.com>
2022-08-04 22:00:20 +0500

    Update products.module.ts

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -15,7 +15,6 @@ import { ProductComponent } from './components/product/product.component';
   ],
   exports: [
     ProductListComponent,
-    ProductComponent
   ]
 })
 export class ProductsModule { }
